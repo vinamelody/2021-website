6225b87eee22be7091e9f595c0bf85e5b4aa719c
Vina Melody <vina_melody@yahoo.com>
2021-01-13 23:55:12 +0900

    Add Software page
    Content to be updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,22 +2,27 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import abductionIllustration from "../images/abduction-illustration.svg";
+import iOSBird from "../images/iosconf-bird.png";
 
-function NotFoundPage() {
+function NotFoundPage () {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <div>
-        <img
-          alt="Ghost getting abducted by aliens"
-          className="block mx-auto w-1/2"
-          src={abductionIllustration}
-        />
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Looks like this page is a ghost that got abducted by aliens...
-        </h2>
-      </div>
+      <section className="w-full">
+        <div className="max-w-4xl mx-auto py-8 px-4 sm:px-6">
+          <img
+            alt="iOS Conference Singapore bird in front of Marina Bay Sands"
+            className="block mx-auto w-1/2"
+            src={iOSBird}
+          />
+          <div className="max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+            <h2 className="bg-orange-400 text-2xl font-normal text-center inline-block my-8 p-3">
+              We cannot find that page you want but looks like this page is where our bird is hiding!
+            </h2>
+          </div>
+        </div>
+      </section>
+
     </Layout>
   );
 }
